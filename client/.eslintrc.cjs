23de68ddff5455901d569e7a3174bdf74c6c979b
Aleksandar <laraluda.g@gmail.com>
2023-10-17 01:41:53 +0200

    basic view and tailwind addded

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -16,3 +16,33 @@ module.exports = {
     ],
   },
 }
+
+
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2021: true },
+//   extends: [
+//     // 'airbnb',
+//     // 'airbnb/hooks',
+//     // 'airbnb-typescript',
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:react-hooks/recommended',
+//     'eslint:recommended'
+//   ],
+//   // parserOptions: {
+//   //   project: './tsconfig.json',
+//   // },
+//   ignorePatterns: ['dist', '.eslintrc.cjs'],
+//   parser: '@typescript-eslint/parser',
+//   plugins: [
+//     'react-refresh',
+//     // 'react',
+//     // '@typescript-eslint'
+//   ],
+//   rules: {
+//     'react-refresh/only-export-components': [
+//       'warn',
+//       { allowConstantExport: true },
+//     ],
+//   },
+// }
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
-import "./App.css";
-import axios from "axios";
-import { useState } from "react";
+import axios from 'axios';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router';
+// Pages
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Account from './pages/Account';
+import Login from './pages/Login';
+// Components
+import Navbar from './components/Navbar';
 
 function App() {
   const [data, setData] = useState();
-  const proxyURL = "/users/v1";
+  const proxyURL = '/users/v1';
 
   async function getData() {
     axios
@@ -17,8 +24,18 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={getData}>Access server using proxy</button>
-      <h1> Data: {data}</h1>
+      <Navbar />
+      <button className="text-3xl font-bold underline" onClick={getData}>Access server using proxy</button>
+      <h1>
+        Data:
+        {data}
+      </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </div>
   );
 }
